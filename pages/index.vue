e6f84a71c6d11c0dd568e341a45f1efe00e7cb15
wingkong1978 <wingkong1978@163.com>
2020-04-28 10:11:46 +0800

    remove map

--- pages/index.vue
+++ pages/index.vue
@@ -53,144 +53,144 @@ export default {
 
       console.log('start')
       const that = this
-      this.$axios.get('https://geo.datav.aliyun.com/areas_v2/bound/100000.json')
-        .then(function (res) {
-          console.info('json-->', res.data)
-          that.$echarts.registerMap('china', res.data)
-          const chart = that.$echarts.init(document.getElementById('myChart'))
-          chart.setOption({
-            series: [{
-              type: 'wordCloud',
+      // this.$axios.get('https://geo.datav.aliyun.com/areas_v2/bound/100000.json')
+      //   .then(function (res) {
+      //     console.info('json-->', res.data)
+      //     that.$echarts.registerMap('china', res.data)
+      const chart = that.$echarts.init(document.getElementById('myChart'))
+      chart.setOption({
+        series: [{
+          type: 'wordCloud',
 
-              // The shape of the "cloud" to draw. Can be any polar equation represented as a
-              // callback function, or a keyword present. Available presents are circle (default),
-              // cardioid (apple or heart shape curve, the most known polar equation), diamond (
-              // alias of square), triangle-forward, triangle, (alias of triangle-upright, pentagon, and star.
+          // The shape of the "cloud" to draw. Can be any polar equation represented as a
+          // callback function, or a keyword present. Available presents are circle (default),
+          // cardioid (apple or heart shape curve, the most known polar equation), diamond (
+          // alias of square), triangle-forward, triangle, (alias of triangle-upright, pentagon, and star.
 
-              shape: 'diamond',
-              mapType: 'china',
+          shape: 'heart',
+          // mapType: 'china',
 
-              // A silhouette image which the white area will be excluded from drawing texts.
-              // The shape option will continue to apply as the shape of the cloud to grow.
+          // A silhouette image which the white area will be excluded from drawing texts.
+          // The shape option will continue to apply as the shape of the cloud to grow.
 
-              // maskImage,
+          // maskImage,
 
-              // Folllowing left/top/width/height/right/bottom are used for positioning the word cloud
-              // Default to be put in the center and has 75% x 80% size.
+          // Folllowing left/top/width/height/right/bottom are used for positioning the word cloud
+          // Default to be put in the center and has 75% x 80% size.
 
-              left: 'center',
-              top: 'center',
-              width: '70%',
-              height: '80%',
-              right: null,
-              bottom: null,
+          left: 'center',
+          top: 'center',
+          width: '70%',
+          height: '80%',
+          right: null,
+          bottom: null,
 
-              // Text size range which the value in data will be mapped to.
-              // Default to have minimum 12px and maximum 60px size.
+          // Text size range which the value in data will be mapped to.
+          // Default to have minimum 12px and maximum 60px size.
 
-              sizeRange: [12, 60],
+          sizeRange: [12, 60],
 
-              // Text rotation range and step in degree. Text will be rotated randomly in range [-90, 90] by rotationStep 45
+          // Text rotation range and step in degree. Text will be rotated randomly in range [-90, 90] by rotationStep 45
 
-              rotationRange: [-90, 90],
-              rotationStep: 45,
+          rotationRange: [-90, 90],
+          rotationStep: 45,
 
-              // size of the grid in pixels for marking the availability of the canvas
-              // the larger the grid size, the bigger the gap between words.
+          // size of the grid in pixels for marking the availability of the canvas
+          // the larger the grid size, the bigger the gap between words.
 
-              gridSize: 8,
+          gridSize: 8,
 
-              // set to true to allow word being draw partly outside of the canvas.
-              // Allow word bigger than the size of the canvas to be drawn
-              drawOutOfBound: false,
+          // set to true to allow word being draw partly outside of the canvas.
+          // Allow word bigger than the size of the canvas to be drawn
+          drawOutOfBound: false,
 
-              // Global text style
-              textStyle: {
-                normal: {
-                  fontFamily: 'sans-serif',
-                  fontWeight: 'bold',
-                  // Color can be a callback function or a color string
-                  color () {
-                  // Random color
-                    return 'rgb(' + [
-                      Math.round(Math.random() * 160),
-                      Math.round(Math.random() * 160),
-                      Math.round(Math.random() * 160)
-                    ].join(',') + ')'
-                  }
-                },
-                emphasis: {
-                  shadowBlur: 10,
-                  shadowColor: '#333'
-                }
-              },
+          // Global text style
+          textStyle: {
+            normal: {
+              fontFamily: 'sans-serif',
+              fontWeight: 'bold',
+              // Color can be a callback function or a color string
+              color () {
+                // Random color
+                return 'rgb(' + [
+                  Math.round(Math.random() * 160),
+                  Math.round(Math.random() * 160),
+                  Math.round(Math.random() * 160)
+                ].join(',') + ')'
+              }
+            },
+            emphasis: {
+              shadowBlur: 10,
+              shadowColor: '#333'
+            }
+          },
 
-              // Data is an array. Each array item must have name and value property.
-              //   data: [{
-              //     name: 'Farrah Abraham',
-              //     value: 1,
-              //     // Style of single text
-              //     textStyle: {
-              //       normal: {},
-              //       emphasis: {}
-              //     }
-              //   },
-              //   {
-              //     name: '22222222222222222',
-              //     value: 2,
-              //     // Style of single text
-              //     textStyle: {
-              //       normal: {},
-              //       emphasis: {}
-              //     }
-              //   },
-              //
-              //   {
-              //     name: '111111',
-              //     value: 3,
-              //     // Style of single text
-              //     textStyle: {
-              //       normal: {},
-              //       emphasis: {}
-              //     }
-              //   },
-              //   {
-              //     name: '44444',
-              //     value: 4
-              //   },
-              //   {
-              //     name: '55555',
-              //     value: 4
-              //   }
-              //
-              //   ]
-              data: that.words
-            }]
-          })
-          //   const
-          //     option = {
-          //       xAxis: {
-          //         type: 'category',
-          //         data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-          //       },
-          //       yAxis: {
-          //         type: 'value'
-          //       },
-          //       series: [{
-          //         data: [820, 932, 901, 934, 1290, 1330, 1320],
-          //         type: 'line'
-          //       }]
+          // Data is an array. Each array item must have name and value property.
+          //   data: [{
+          //     name: 'Farrah Abraham',
+          //     value: 1,
+          //     // Style of single text
+          //     textStyle: {
+          //       normal: {},
+          //       emphasis: {}
           //     }
-          //   chart.setOption(option)
-          //   console.log('chart----->', chart)
+          //   },
+          //   {
+          //     name: '22222222222222222',
+          //     value: 2,
+          //     // Style of single text
+          //     textStyle: {
+          //       normal: {},
+          //       emphasis: {}
+          //     }
+          //   },
+          //
+          //   {
+          //     name: '111111',
+          //     value: 3,
+          //     // Style of single text
+          //     textStyle: {
+          //       normal: {},
+          //       emphasis: {}
+          //     }
+          //   },
+          //   {
+          //     name: '44444',
+          //     value: 4
+          //   },
+          //   {
+          //     name: '55555',
+          //     value: 4
+          //   }
+          //
+          //   ]
+          data: that.words
+        }]
+      })
+      //   const
+      //     option = {
+      //       xAxis: {
+      //         type: 'category',
+      //         data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      //       },
+      //       yAxis: {
+      //         type: 'value'
+      //       },
+      //       series: [{
+      //         data: [820, 932, 901, 934, 1290, 1330, 1320],
+      //         type: 'line'
+      //       }]
+      //     }
+      //   chart.setOption(option)
+      //   console.log('chart----->', chart)
 
-          // chart.setOption({
-          //   series: [{
-          //     type: 'map',
-          //     map: 'china'
-          //   }]
-          // })
-        })
+      // chart.setOption({
+      //   series: [{
+      //     type: 'map',
+      //     map: 'china'
+      //   }]
+      // })
+      // })
       // const chart = echarts.init(document.getElementById('myChart'))
     }
   }
